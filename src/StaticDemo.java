01c4826910dda5dfd742254f881baab0d2878ede
ishansivmal <ishansivmal@gmail.com>
2026-02-07 12:36:22 +0530

    static method and varible

---
+++ src/StaticDemo.java
@@ -0,0 +1,8 @@
+public class StaticDemo {
+    public static void main(String[] args) {
+        System.out.println(StaticVarible.Name);
+        System.out.println(StaticVarible.Age);
+        System.out.println(StaticVarible.Address);
+        StaticVarible.getINfor();
+    }
+}
---
+++ src/StaticVarible.java
@@ -0,0 +1,13 @@
+public class StaticVarible {
+    static  String  Name = "ishan";
+    static   int Age = 23;
+    static  String Address = " aruna" +
+            "yahalabedda, haputhale";
+
+    public static void   getINfor()
+    {
+        System.out.println("Name : " + Name);
+        System.out.println("Age : " + Age);
+        System.out.println("Address : " + Address);
+    }
+}
