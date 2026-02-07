71f7927e1d52a37890346100171bb370f65c2328
ishansivmal <ishansivmal@gmail.com>
2026-02-07 16:22:10 +0530

    add abstract class AB and implementation in insidepackage

---
+++ src/ishan/AB.java
@@ -0,0 +1,10 @@
+package ishan;
+
+public abstract class AB {
+    public  String Name = "ishan";
+    public  int Age = 20;
+    public  String Address = "Sri Lanka";
+
+    public abstract  void getInfor();
+
+}
---
+++ src/ishan/insidepackage.java
@@ -0,0 +1,13 @@
+package ishan;
+
+public class insidepackage extends AB {
+
+    @Override
+    public void getInfor() {
+        public void getInfor() {
+            System.out.println("Name: " + Name);
+            System.out.println("Age: " + Age);
+            System.out.println("Address: " + Address);
+        }
+    }
+}
