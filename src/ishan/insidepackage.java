d15b1b56a57350cc2253168c96e450af91bd26f2
ishansivmal <ishansivmal@gmail.com>
2026-02-07 17:22:24 +0530

    add access modifier and encapsulation examples

--- src/ishan/insidepackage.java
+++ src/ishan/insidepackage.java
@@ -4,10 +4,10 @@ public class insidepackage extends AB {
 
     @Override
     public void getInfor() {
-        public void getInfor() {
+
             System.out.println("Name: " + Name);
             System.out.println("Age: " + Age);
             System.out.println("Address: " + Address);
-        }
+
     }
 }
